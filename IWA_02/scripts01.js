81dfcad0f5881795a967d32a9adba7d9f08615ba
onwabasapepa <137301840+onwabasapepa@users.noreply.github.com>
2023-08-03 19:52:56 +0200

    Uploaded IWA_02
    Uploaded IWA_02

---
+++ IWA_02/scripts01.js
@@ -0,0 +1,7 @@
+/* This is the first name of the user*/
+const user = 'John'
+
+/* This is the last name of the user*/
+const surname = 'Smith'
+
+console.log(user, surname);
---
+++ IWA_02/scripts03.js
@@ -0,0 +1,13 @@
+
+/* 
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+const warn = "Security scan starting"
+
+/* It is important to let the user know when they can close the page.*/
+const info = "Please wait for scan to complete before closing the browser"
+
+console.warn(warn, info);
