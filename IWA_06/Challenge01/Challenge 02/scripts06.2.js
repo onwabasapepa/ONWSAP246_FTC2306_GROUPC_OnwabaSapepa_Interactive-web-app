f20744d8d9240b10fd2072733f3b42d8219179dc
onwabasapepa <137301840+onwabasapepa@users.noreply.github.com>
2023-08-16 14:56:40 +0200

    uploaded IWA_06
    Fellow student

---
+++ IWA_06/Challenge01/Challenge 02/scripts06.2.js
@@ -0,0 +1,24 @@
+const rent = 400;
+const tax = '8%';
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
+
+// Only change below this line
+
+/*if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
+	const taxAsDecimal = tax / '100'
+  const startingAfterTax = salary * '1' - taxAsDecimal
+	const balace = starting - transport - food - rent
+}
+	
+console.log(balance.toFixed(3))*/
+let balance = 0;
+if ((hourOfDay == 0) && (minuteOfDay == 0)){
+    const taxAsDecimal = parseInt(tax) / 100;
+    const startingAfterTax = salary * (1 - taxAsDecimal);
+    balance = 'R ' + (startingAfterTax - (food + transport + rent)).toFixed(2);
+}
+console.log(balance);
---
+++ IWA_06/Challenge01/scripts06.js
@@ -0,0 +1,10 @@
+const primaryPhone = 'O748105141'
+const secondaryPhone = '0219131568'
+
+// Only change below this line
+
+const primaryValid = parseInt(primaryPhone)
+const secondaryValid = parseInt(secondaryPhone)
+
+console.log('Primary phone is valid numerical string:',Boolean(primaryValid))
+console.log('Secondary phone is valid numerical string:',Boolean(secondaryValid))
